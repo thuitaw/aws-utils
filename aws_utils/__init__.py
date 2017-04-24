ebd6c4e8544d0c381a458f6e53e6e51ccb0a5d4b
Thuita Wachira <tw@refunite.org>
2017-04-24 15:01:31 +0000

    upgraded aws utils

--- aws_utils/__init__.py
+++ aws_utils/__init__.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python
 
-from .commandlineparser import CommandLineParser #noqa
-from .ini_config_loader import ( #noqa
-    MissingConfigError, #noqa
-    MissingConfigSectionError, #noqa
-    MissingConfigOptionError, #noqa
-    INIConfigLoader, #noqa
-    mk_config) #noqa
-
-from .sns_pubsub_mixin import SNSPubSubMixin #noqa
-from .sqs_consumer import SQSConsumer #noqa
-from .s3_mixin import S3Mixin #noqa
+from .sns_pubsub_connector import SNSPubSubConnector  # noqa
+from .sqs_consumer import SQSConsumer  # noqa
+from .s3_connector import S3Connector  # noqa
 
 from .exceptions import ( # noqa
     InvalidBucketNameError, BucketCreationError,
-    NonExistentBucketError, MissingFileError, NonExistentKeyError) # noqa
+    NonExistentBucketError, MissingFileError, NonExistentKeyError,
+    MissingQueueException)
