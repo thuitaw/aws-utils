cf9b5fdfcf7eef308579640b1b87216257486505
Thuita Wachira <tw@refunite.org>
2017-04-24 13:59:13 +0000

    added setup.ini

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!./env/bin/python
+from __future__ import absolute_import
+from setuptools import setup, find_packages
+
+
+setup(
+    name='aws-utils',
+    version='0.1',
+    author="Thuita Wachira",
+    url="https://github.com/thuitaw/aws-utils.git",
+    keywords="utilities utils aws ",
+    description="aws utilities package",
+    include_package_data=True,
+    packages=find_packages(exclude=("build")),
+    license='GPL',
+    test_suite="test",
+    install_requires=["boto"],
+
+)
